e729f405fc105c5889e0c4705de53ba855a6a3df
weirui <weirui@wps.cn>
2026-01-04 01:09:56 +0800

    添加json支持

--- build.rs
+++ build.rs
@@ -24,26 +24,23 @@ fn main() {
         proto_files.push(format!("{}/{}.proto", module, module));
     }
 
-    // 配置 prost-build，添加 serde 支持
-    let mut config = Config::new();
-    
-    // 为所有类型添加 serde 支持，使用 camelCase 作为默认命名规则
+    // 配置 prost-build 以启用 serde 支持
+    let mut config = prost_build::Config::new();
+
+    // 为消息类型添加 serde 注解
+
     let messages = vec![
-    "openim.sdkws.FriendInfo",
-    "openim.sdkws.UserInfo",
-    "openim.sdkws.PublicUserInfo",
-    "openim.sdkws.FriendRequest",
-    "openim.sdkws.BlackInfo",
-];
+        "openim.sdkws.FriendInfo",
+        "openim.sdkws.UserInfo",
+        "openim.sdkws.MsgData",
+        "openim.sdkws.OfflinePushInfo",
+    ];
     for message in messages {
         config.type_attribute(message, "#[derive(serde::Serialize, serde::Deserialize)]");
         config.type_attribute(message, "#[serde(rename_all = \"camelCase\")]");
     }
-    
-    // 为 sdkws 模块的特殊字段配置 JSON 字段名映射（覆盖 camelCase 规则）
-    // 这些字段需要 ID/URL 等全大写，而不是 camelCase
-    
-    // FriendInfo 字段
+
+    // 为 FriendInfo.ownerUserID 字段添加 serde 注解
     config.field_attribute(
         "openim.sdkws.FriendInfo.ownerUserID",
         "#[serde(rename = \"ownerUserID\")]",
@@ -52,68 +49,12 @@ fn main() {
         "openim.sdkws.FriendInfo.operatorUserID",
         "#[serde(rename = \"operatorUserID\")]",
     );
-    
-    // UserInfo 字段
     config.field_attribute(
         "openim.sdkws.UserInfo.userID",
         "#[serde(rename = \"userID\")]",
     );
-    config.field_attribute(
-        "openim.sdkws.UserInfo.faceURL",
-        "#[serde(rename = \"faceURL\")]",
-    );
-    config.field_attribute(
-        "openim.sdkws.UserInfo.app_manger_level",
-        "#[serde(rename = \"appMangerLevel\")]",
-    );
-    config.field_attribute(
-        "openim.sdkws.UserInfo.global_recv_msg_opt",
-        "#[serde(rename = \"globalRecvMsgOpt\")]",
-    );
-    
-    // PublicUserInfo 字段
-    config.field_attribute(
-        "openim.sdkws.PublicUserInfo.user_id",
-        "#[serde(rename = \"userID\")]",
-    );
-    config.field_attribute(
-        "openim.sdkws.PublicUserInfo.face_url",
-        "#[serde(rename = \"faceURL\")]",
-    );
-    
-    // FriendRequest 字段
-    config.field_attribute(
-        "openim.sdkws.FriendRequest.from_user_id",
-        "#[serde(rename = \"fromUserID\")]",
-    );
-    config.field_attribute(
-        "openim.sdkws.FriendRequest.from_face_url",
-        "#[serde(rename = \"fromFaceURL\")]",
-    );
-    config.field_attribute(
-        "openim.sdkws.FriendRequest.to_user_id",
-        "#[serde(rename = \"toUserID\")]",
-    );
-    config.field_attribute(
-        "openim.sdkws.FriendRequest.to_face_url",
-        "#[serde(rename = \"toFaceURL\")]",
-    );
-    config.field_attribute(
-        "openim.sdkws.FriendRequest.handler_user_id",
-        "#[serde(rename = \"handlerUserID\")]",
-    );
-    
-    // BlackInfo 字段
-    config.field_attribute(
-        "openim.sdkws.BlackInfo.owner_user_id",
-        "#[serde(rename = \"ownerUserID\")]",
-    );
-    config.field_attribute(
-        "openim.sdkws.BlackInfo.operator_user_id",
-        "#[serde(rename = \"operatorUserID\")]",
-    );
-    
-    // 编译 proto 文件
+
+    // 使用 prost-build 编译所有 proto 文件
     config
         .compile_protos(&proto_files, &["."])
         .expect("Failed to compile protos");
@@ -123,4 +64,3 @@ fn main() {
         println!("cargo:rerun-if-changed={}/{}.proto", module, module);
     }
 }
-
